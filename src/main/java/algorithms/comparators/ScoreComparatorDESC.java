8f64d3d2c3f9f06c68e2b710800704dcbdbfddf0
Trymken <lewbsrox1@yadnex.ru>
2022-06-09 16:22:53 +0800

    added handling of the case when score is not a number

--- src/main/java/algorithms/comparators/ScoreComparatorDESC.java
+++ src/main/java/algorithms/comparators/ScoreComparatorDESC.java
@@ -8,6 +8,17 @@ public class ScoreComparatorDESC implements Comparator<Row> {
 
     @Override
     public int compare(Row o1, Row o2) {
-        return -Double.compare(Double.parseDouble(o1.getScore().get(0)), Double.parseDouble(o2.getScore().get(0)));
+        double x1, x2;
+        try {
+            x1 =  Double.parseDouble(o1.getScore().get(0));
+        } catch (Exception e){
+            x1 = 0.0;
+        }
+        try {
+            x2 = Double.parseDouble(o2.getScore().get(0));
+        } catch (Exception e){
+            x2 = 0.0;
+        }
+        return -Double.compare(x1, x2);
     }
 }
